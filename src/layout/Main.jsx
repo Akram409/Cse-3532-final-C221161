a8ffb9e07e19c5628d1df53a529f407cc4eb8d5e
Akram409 <akram.iiuc.ctg@gmail.com>
2025-01-18 14:53:51 +0600

    updated

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Share/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <div className="">
-        {/* <Navbar /> */}
+      <div className="container mx-auto">
+        <Navbar />
       </div>
       <div >
         <Outlet />
